1dadf7e422f5f39af0d5dd193a522d7f4e8c643d
Ritchie <ac.ritchiew@gmail.com>
2018-10-30 13:31:46 -0400

    fix several logic errors

--- maxInfluence/dynamicAssign.cu
+++ maxInfluence/dynamicAssign.cu
@@ -15,7 +15,7 @@
 #define MAX_NODE 10000
 #define THREAD (4 * 256)
 
-float CONSTANT_PROBABILITY = 0.01;
+float CONSTANT_PROBABILITY = 0.05;
 int _adjMat[MAX_NODE][MAX_OUT];
 
 // read graph information from files and generate a adjacent list for representation
@@ -63,13 +63,20 @@ __device__ int findVertice(int* nodeSet, int nodes){
 }
 
 // judge if node 'nd' is visited once
-__device__ bool nd_isVisited(bool* vis, int nd, int index){
-    return vis[index * THREAD + nd];
+__device__ bool nd_isVisited(bool* vis, int nd, int tot, int index){
+    return vis[index * tot + nd];
 }
 
 // set node 'nd' visited
-__device__ void nd_setVisited(bool* vis, int nd, int index){
-    vis[index * THREAD + nd] = true;
+__device__ void nd_setVisited(bool* vis, int nd, int tot, int index){
+    vis[index * tot + nd] = true;
+}
+
+// set all node invisited, can be optimized
+__device__ void nd_resetVisited(bool* vis, int tot, int index){
+    int i = index * tot, end = index * tot + tot;
+    for(;i < end;i++)
+        vis[i] = false;
 }
 
 // initialize random seeds for each thread
@@ -95,19 +102,21 @@ __global__ void bfs(int totalNodes,
     float randProb;
     curandState localState = state[index];
     while((node = findVertice(nodeSet, totalNodes)) != -1){
+        count = 0;
         from = node;
         que_init(que_h, que_t, index);
+        nd_resetVisited(closed, totalNodes, index);
         if(!que_enque(queue, que_h, que_t, from, index));   // in case queue overflow
-        nd_setVisited(closed, from, index);
+        nd_setVisited(closed, from, totalNodes, index);
         while(!que_isEmpty(que_h, que_t, index)){
             node = que_deque(queue, que_h, que_t, index);
             adjNode = adjCount[node];
             while(adjNode < adjCount[node + 1]){
-                if(!nd_isVisited(closed, adjList[adjNode], index)){
+                if(!nd_isVisited(closed, adjList[adjNode], totalNodes, index)){
                     randProb = curand_uniform(&localState);
                     if(randProb < constProb){
                         if(!que_enque(queue, que_h, que_t, adjList[adjNode], index));
-                        nd_setVisited(closed, adjList[adjNode], index);
+                        nd_setVisited(closed, adjList[adjNode], totalNodes, index);
                         count++;
                     }
                 }
@@ -125,19 +134,29 @@ int h_adjList[MAX_NODE * MAX_OUT];
 int h_nodeSet[MAX_NODE];
 
 // for argument parsing
-char short_options[] = "p::o";
+char short_options[] = "p::vto";
 struct option long_options[] = {
     {"probability", optional_argument, 0, 'p'},
+    {"verbose", no_argument, 0, 'v'},
+    {"timeonly", no_argument, 0, 't'},
     {"output", no_argument, 0, 'o'}
 };
 
 int main(int argc, char** argv){
     // argument parsing
     char ch;
+    bool verbose = false, timeonly = false;
     while((ch = getopt_long(argc, argv, short_options, long_options, NULL)) != -1){
         switch(ch){
         case 'p':
-            CONSTANT_PROBABILITY = atof(optarg);
+            CONSTANT_PROBABILITY *= atoi(optarg);
+            CONSTANT_PROBABILITY = CONSTANT_PROBABILITY > 1 ? 1. : CONSTANT_PROBABILITY;
+            break;
+        case 'v':
+            verbose = true;
+            break;
+        case 't':
+            timeonly = true;
             break;
         case 'o':
             freopen("../outputs/dynamicOutput.txt", "a", stdout);
@@ -150,9 +169,11 @@ int main(int argc, char** argv){
     int totalEdges = readGraph("../data/wiki.txt", h_adjCount, h_adjList, totalNodes, maxOutDegree);
     if(totalEdges < 0)
         return 0;
-    printf("========= NEW RUN\n");
-    printf("This graph contains %d nodes connected by %d edges\n", totalNodes, totalEdges);
-    printf("Set constant probability: %.2f\n", CONSTANT_PROBABILITY);
+    if(!timeonly){
+        printf("========= NEW RUN\n");
+        printf("This graph contains %d nodes connected by %d edges\n", totalNodes, totalEdges);
+        printf("Set constant probability: %.2f\n", CONSTANT_PROBABILITY);
+    }
 
     // addresses for GPU memory addresses storage
     bool* d_closed;
@@ -212,13 +233,20 @@ int main(int argc, char** argv){
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&gpu_runtime, start, stop);
 
-    cudaMemcpy(h_nodeSet, d_nodeSet, sizeof(int) * totalNodes + 1, cudaMemcpyDeviceToHost);
-
     // statistics
-    for(int i = 0;i < totalNodes;i++)
-        if(h_nodeSet[i] > 0)
-            printf("influence of node %d: %d\n", i, h_nodeSet[i]);
-    printf("========= GPU ELAPSED TIME: %f ms\n\n", gpu_runtime);
+    if(verbose && !timeonly){
+        cudaMemcpy(h_nodeSet,
+                   d_nodeSet,
+                   sizeof(int) * totalNodes + 1,
+                   cudaMemcpyDeviceToHost);
+        for(int i = 0;i < totalNodes;i++)
+            if(h_nodeSet[i] > 0)
+                printf("influence of node %d: %d\n", i, h_nodeSet[i]);
+    }
+    if(!timeonly)
+        printf("========= GPU ELAPSED TIME: %f ms\n\n", gpu_runtime);
+    else
+        printf("%f\n", gpu_runtime);
 
     // cuda memory free
     cudaEventDestroy(start);
