48ce68a410f6ae4b312432cedd2ba5869274e89f
Ruiqi Wang <wangrq@udel.edu>
2019-06-18 12:05:35 -0400

    include head files

--- CompetitiveCascade/src/tools.hpp
+++ CompetitiveCascade/src/tools.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <chrono>
 #include <vector>
 
 bool intersection(const std::set<int> &l1, const std::set<int> &l2){
@@ -26,12 +27,12 @@ void printContainer(const T &container){
     std::cout << std::endl;
 }
 
-void printTime(chrono::high_resolution_clock::time_point start, chrono::high_resolution_clock::time_point end){
+void printTime(std::chrono::high_resolution_clock::time_point start, std::chrono::high_resolution_clock::time_point end){
     auto duration = end-start;
-    auto hour = chrono::duration_cast<chrono::hours>(duration).count();
-    auto min = chrono::duration_cast<chrono::minutes>(duration).count();
-    auto sec = chrono::duration_cast<chrono::seconds>(duration).count();
-    cout << "time elapsed: " << hour << " hours " << min%60 << " minutes " << sec%60 << " seconds" << endl;
+    auto hour = std::chrono::duration_cast<std::chrono::hours>(duration).count();
+    auto min = std::chrono::duration_cast<std::chrono::minutes>(duration).count();
+    auto sec = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+    std::cout << "time elapsed: " << hour << " hours " << min%60 << " minutes " << sec%60 << " seconds" << std::endl;
 }
 
 struct Results{
